8a13fea2a3b3f867fe4e5b6a72d541baa95896b8
David Castro <47994850+davidcastro96@users.noreply.github.com>
2021-02-10 11:47:19 -0500

    Update

--- pages/vuelos.js
+++ pages/vuelos.js
@@ -1,24 +1,14 @@
 import Link from "next/link";
 import Container from "../components/Container";
 import fetch from "isomorphic-fetch";
-import Flights from "../components/Flights";
 
 const Vuelos = (props) => {
   console.log(props);
   return (
     <Container>
       <h1>Estos son los siguientes vuelos</h1>
-      <Flights flights={props.flights} />
     </Container>
   );
-};
-
-Vuelos.getInitialProps = async (ctx) => {
-  const res = await fetch(
-    "https://intranet.tiquetesytiquetes.com/testeos/r_controller.php?itinerarios=MO4737"
-  );
-  const dato = await res.json();
-  return { flights: dato };
 };
 
 export default Vuelos;
