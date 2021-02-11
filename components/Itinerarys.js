cea2d000b59724ca251555e85b9097ba7f5ea421
davidcastro96 <davidcastro0796@outlook.es>
2021-02-11 00:46:53 -0500

    Update Itinerarys.js

--- components/Itinerarys.js
+++ components/Itinerarys.js
@@ -2,27 +2,23 @@ const Itinerarys = (props) => {
   return (
     <div className="container pt-3">
       {props.flights.map((flight) => (
-        <div className="row row-cols-12">
-          <li className="list-group-item list-group-item-action">
-            <div className="row">
-              <div className="col">
-                <li>dsds</li>
-                <li>dsds</li>
-                <li>dsds</li>
-              </div>
-              <div className="col">
-                <li>dsds</li>
-                <li>dsds</li>
-                <li>dsds</li>
-              </div>
-              <div className="col">
-                <li>dsds</li>
-                <li>dsds</li>
-                <li>dsds</li>
-              </div>
-            </div>
-          </li>
-        </div>
+        <table border="1px">
+          <tbody>
+            <tr>
+              <td rowSpan={3}>Campo unificado</td>
+              <td>Campo 2</td>
+              <td>Campo 3</td>
+            </tr>
+            <tr>
+              <td>Campo 5</td>
+              <td>Campo 6</td>
+            </tr>
+            <tr>
+              <td>Campo 8</td>
+              <td>Campo 9</td>
+            </tr>
+          </tbody>
+        </table>
       ))}
     </div>
   );
