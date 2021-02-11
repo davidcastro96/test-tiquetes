79dd565fa0209663a3735dc5cd6ad5f18f86b2d4
David Castro <47994850+davidcastro96@users.noreply.github.com>
2021-02-11 12:06:47 -0500

    public

--- components/Itinerarys.js
+++ components/Itinerarys.js
@@ -1,25 +1,37 @@
 const Itinerarys = (props) => {
+
+  var bgreen = {
+    backgroundColor: '000',
+  };
+
   return (
-    <div className="container pt-3">
-      {props.flights.map((flight) => (
-        <table border="1px">
-          <tbody>
-            <tr>
-              <td rowSpan={3}>Campo unificado</td>
-              <td>Campo 2</td>
-              <td>Campo 3</td>
+    <div className="pt-3">
+      <table className="table table-striped">
+        <tbody><tr>
+            <th rowSpan="5" className="title" style={{
+        backgroundColor: '#acd038',
+        textAlign: 'center',
+        paddingTop: '20%',
+        color: '#fff',
+        fontSize: '20px',
+      }}>Información de vuelos</th></tr>
+            {props.flights.map((flight) => (
+              <tr>
+              <td>
+              <ul className="mt-0 mb-0 p-0"><b>Aerolínea</b> &nbsp; &nbsp; {flight.aerolinea_validadora}</ul>
+              <ul className="mt-0 mb-0 p-0"><b>Salida</b> &nbsp; &nbsp; {flight.FechaSalida}</ul>
+              <ul className="mt-0 mb-0 p-0"><b>Hora</b> &nbsp; &nbsp; {flight.HoraSalida}</ul>
+              <ul className="mt-0 mb-0 p-0"><b>No. de vuelo</b> &nbsp; &nbsp; {flight.numero_vuelo}</ul>
+            </td>
+            <td>
+              <ul className="mt-0 mb-0 p-0" >&nbsp;</ul>
+              <ul className="mt-0 mb-0 p-0"><b>Llegada</b> &nbsp; &nbsp; {flight.FechaLLegada}</ul>
+              <ul className="mt-0 mb-0 p-0"><b>Hora</b> &nbsp; &nbsp; {flight.HoraLLegada}</ul>
+              <ul className="mt-0 mb-0 p-0"><b>Ruta</b> &nbsp; &nbsp; {flight.origen} - {flight.destino}</ul>
+            </td>
             </tr>
-            <tr>
-              <td>Campo 5</td>
-              <td>Campo 6</td>
-            </tr>
-            <tr>
-              <td>Campo 8</td>
-              <td>Campo 9</td>
-            </tr>
-          </tbody>
-        </table>
-      ))}
+            ))}
+        </tbody></table>
     </div>
   );
 };
